3636b46a36c9aa7d466480538eef3d416fd43cdd
David Cortez <david97ly@hotmail.com>
2015-06-12 14:24:22 -0600

    veamos ahora

--- django_project/urls.py
+++ django_project/urls.py
@@ -26,6 +26,6 @@ urlpatterns = patterns('',
 if settings.DEBUG:
     urlpatterns += patterns("",
         url(r'^media/(?P<path> *)$','django.views.static.serve',
-            {'document_root':'media/photos',}
+            {'document_root':'media',}
             ),
     )  
