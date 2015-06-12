48df1bf6a058a0e81a5ad5003f02d1c116d2df40
David Cortez <david97ly@hotmail.com>
2015-06-12 15:29:07 -0600

    como anteriormente

--- django_project/settings.py
+++ django_project/settings.py
@@ -92,8 +92,3 @@ STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT= os.path.join(BASE_DIR,'media')
 
-STATICFILES_DIRS = (
-    # Put strings here, like "/home/html/static" or "C:/www/django/static".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
-    os.path.join(BASE_DIR,'static'),
