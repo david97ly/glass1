f674e72362534ad54e28f443af402df7442e90cf
David Cortez <david97ly@hotmail.com>
2015-06-12 14:56:36 -0600

    con la ruta del statico

--- django_project/settings.py
+++ django_project/settings.py
@@ -87,7 +87,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 STATIC_ROOT = '/home/django/django_project/glass/static/'
 STATIC_URL = '/static/'
-
+STATICFILES_DIRS = [os.path.join(BASE_DIR,'templates')]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT= os.path.join(BASE_DIR,'media')
